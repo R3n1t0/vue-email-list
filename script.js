3028cf61c25b394818302de3649a2ccf03c66d10
R3n1t0 <98884076+R3n1t0@users.noreply.github.com>
2022-05-11 15:29:44 +0200

    mail generati

--- script.js
+++ script.js
@@ -6,14 +6,30 @@ const app = new Vue({
 
   data: {
 
+    maiList: []
 
   },
 
 
   methods:{
 
-    
+    generaMaiList(){
+      for (let i = 0; i < 10 ; i++) {
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail") 
+          .then((res)=>{
+          this.maiList = res.data.response
+          console.log(this.maiList);
+        })
+      }
+    }
 
-  }
+  },
+
+  mounted(){
+
+    this.generaMaiList()
+
+  },
+
 
 })
