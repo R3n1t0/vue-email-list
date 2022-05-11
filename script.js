2e43522aec78b468c12c730abda5ba9434ddd0ea
R3n1t0 <98884076+R3n1t0@users.noreply.github.com>
2022-05-11 15:53:22 +0200

    commenti

--- script.js
+++ script.js
@@ -6,24 +6,29 @@ const app = new Vue({
 
   data: {
 
-    maiList: []
+    maiList: [],
+
+    loading: true
 
   },
 
 
   methods:{
 
+    /* Funzione per generare una mailing list di 10 mail */
     generaMaiList(){
+
       for (let i = 0; i < 10 ; i++) {
+        
         axios.get("https://flynn.boolean.careers/exercises/api/random/mail") 
         .then((res)=>{
 
-          this.maiList.push(res.data.response) 
+          this.maiList.push(res.data.response);
           
         })
 
-      }
-
+      } 
+      
     }
 
   },
