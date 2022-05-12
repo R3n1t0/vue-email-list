4658f12e01abacfc7e7fc6f7c75c30993d6bf48c
R3n1t0 <98884076+R3n1t0@users.noreply.github.com>
2022-05-12 10:08:23 +0200

    corretto

--- script.js
+++ script.js
@@ -6,9 +6,17 @@ const app = new Vue({
 
   data: {
 
+    apiUrl: "https://flynn.boolean.careers/exercises/api/random/mailx",
+
     maiList: [],
 
-    loading: true
+    totalMails: 10,
+
+    isLoading: true,
+
+    isError: false,
+
+    errorMessage: ""
 
   },
 
@@ -18,24 +26,27 @@ const app = new Vue({
     /* Funzione per generare una mailing list di 10 mail */
     generaMaiList(){
 
-      for (let i = 0; i < 10 ; i++) {
+      for (let i = 0; i < this.totalMails ; i++) {
 
-        axios.get("https://flynn.boolean.careers/exercises/api/random/mail") 
+        axios.get(this.apiUrl) 
         .then((res)=>{
 
           this.maiList.push(res.data.response);
+          if(this.maiList.length === this.totalMails){
+            this.isLoading = false
+          }
           
+        })
+
+        .catch((error)=>{
+
+          this.errorMessage = error;
+          this.isError = true
+
         })
 
       }
 
-      setTimeout(()=>{
-
-        this.loading = false
-
-      },3000)
-
- 
     }
 
   },
